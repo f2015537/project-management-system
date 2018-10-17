9844abf0edb71cfa6ba2aff32c0ee70cdcb66120
f2015537 <divyampatro1997@gmail.com>
2018-10-17 23:39:36 +0530

    Hooked up the databse

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,9 +1,20 @@
 export const createProject = (project) => {
     return (dispatch, getState,{getFirestore,getFirebase}) => {
-        
-        dispatch({
-            type: 'CREATE_PROJECT',
-            project,
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Divyam',
+            authorLastName: 'Patro',
+            authorId: 12345,
+            createdAt: new Date(),
+        }).then(() => {
+            dispatch({
+                type: 'CREATE_PROJECT',
+                project,
+            })
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err})
         })
+
     }
 }
