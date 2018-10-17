7a12e7fa74faa306a8476755d80ab0cf987d183f
f2015537 <divyampatro1997@gmail.com>
2018-10-17 23:18:05 +0530

    Some complex backend/server stuff and some middleware added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore,applyMiddleware } from 'redux';
+import { createStore,applyMiddleware,compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { getFirestore,reduxFirestore } from 'redux-firestore'
+import { getFirebase,reactReduxFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbconfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+    compose(
+    applyMiddleware(thunk.withExtraArgument({
+    getFirestore,getFirebase
+})),
+reduxFirestore(fbConfig),
+reactReduxFirebase(fbConfig)
+
+));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,6 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState,{getFirestore,getFirebase}) => {
+        
         dispatch({
             type: 'CREATE_PROJECT',
             project,
