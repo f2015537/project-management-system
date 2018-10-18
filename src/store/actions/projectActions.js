a7532b777e02c57a91c8f9e0e3f3cbfa405b8703
f2015537 <divyampatro1997@gmail.com>
2018-10-19 02:01:44 +0530

    Improved create project component and project summary component

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,11 +1,13 @@
 export const createProject = (project) => {
     return (dispatch, getState,{getFirestore,getFirebase}) => {
         const firestore = getFirestore();
+        const profile = getState().firebase.profile
+        const authorId = getState().firebase.auth.uid
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Divyam',
-            authorLastName: 'Patro',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date(),
         }).then(() => {
             dispatch({
