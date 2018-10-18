6d27f437304a723d3321031d6494259a50faa958
f2015537 <divyampatro1997@gmail.com>
2018-10-18 17:15:43 +0530

    Some UI improvements

--- src/index.js
+++ src/index.js
@@ -17,13 +17,17 @@ const store = createStore(rootReducer,
     getFirestore,getFirebase
 })),
 reduxFirestore(fbConfig),
-reactReduxFirebase(fbConfig)
+reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
 
 ));
 
+store.firebaseAuthIsReady.then(() => {
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+})
+
+
