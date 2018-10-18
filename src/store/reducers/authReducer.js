1444b010aa4260e0a36a52bbb1f1f9694c043339
f2015537 <divyampatro1997@gmail.com>
2018-10-18 16:20:14 +0530

    Hooked up signout with firebase

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,26 +1,27 @@
 const initState = {
     authError: null
-}
-
-const authReducer = (state = initState, action) => {
-    switch (action.type) {
-        case 'LOGIN_ERROR':
-        console.log('login error')    
+  }
+  
+  const authReducer = (state = initState, action) => {
+    switch(action.type){
+      case 'LOGIN_ERROR':
+        console.log('login error');
         return {
-                ...state,
-                authError: 'Login failed'
-            }
-
-        case 'LOGIN_SUCCESS':
-        console.log('login success')    
+          ...state,
+          authError: 'Login failed'
+        }
+      case 'LOGIN_SUCCESS':
+        console.log('login success');
         return {
-                ...state,
-                authError: null
-            }
-    
-        default:
-            return state;
+          ...state,
+          authError: null
+        }
+      case 'SIGNOUT_SUCCESS':
+        console.log('signout success');
+        return state
+      default:
+        return state
     }
-}
-
-export default authReducer
+  };
+  
+  export default authReducer;
