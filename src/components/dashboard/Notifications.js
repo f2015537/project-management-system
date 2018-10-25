952bfec814bfca126860d909c405891b89e4854f
f2015537 <divyampatro1997@gmail.com>
2018-10-26 03:51:28 +0530

    Notifications are now shown in real time

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import moment from 'moment'
 
 const Notifications = (props) => {
+  const { notifications } = props;
   return (
-    <div>
-      <div className="section">
-        <div className="card">
-          <div className="card z-depth-0">
-            <div className="card-content">
-              <span className="card-title">Notofications</span>
-              <ul className="notifications">
-                <li>Notification</li>
-                <li>Notification</li>
-                <li>Notification</li>
-                <li>Notification</li>
-              </ul>
-            </div>
-          </div>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            { notifications && notifications.map(item =>{
+              return <li key={item.id}>
+                <span className="pink-text">{item.user} </span>
+                <span>{item.content}</span>
+                <div className="note-date grey-text">{moment(item.time.toDate()).fromNow()}</div>
+              </li>
+            })}
+          </ul>
         </div>
       </div>
     </div>
