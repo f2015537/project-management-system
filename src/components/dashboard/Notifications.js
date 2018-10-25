64364336c94b4083e1ca6339299cb990b5515cb5
f2015537 <divyampatro1997@gmail.com>
2018-10-25 20:59:15 +0530

    Added some static data to the notifications component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Notifications = () => {
+const Notifications = (props) => {
   return (
     <div>
-      <p>Notifications</p>
+      <div className="section">
+        <div className="card">
+          <div className="card z-depth-0">
+            <div className="card-content">
+              <span className="card-title">Notofications</span>
+              <ul className="notifications">
+                <li>Notification</li>
+                <li>Notification</li>
+                <li>Notification</li>
+                <li>Notification</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
